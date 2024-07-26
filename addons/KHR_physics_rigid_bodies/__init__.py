5002af82e3443f118fc78335886e84b9ccb83875
Eoin Mcloughlin <hkeoin@eoinrul.es>
2024-07-26 11:16:59 +0100

    Improve visual display of options

--- addons/KHR_physics_rigid_bodies/__init__.py
+++ addons/KHR_physics_rigid_bodies/__init__.py
@@ -20,15 +20,18 @@ bl_info = {
 def draw(context, layout):
     exportProps = bpy.context.scene.khr_physics_exporter_props
     header, body = layout.panel("KHR_physics_rigid_bodies_exporter", default_closed=False)
+    header.use_property_split = False
     header.prop(exportProps, 'enabled')
     header.active = exportProps.enabled
     if body != None:
+        body.use_property_split = False
         body.prop(exportProps, "reparent_bones")
 
     # todo.eoin Need to figure out how to determine if we're in the import or export window
     # Currently both property panels are displayed in both windows
     importProps = bpy.context.scene.khr_physics_importer_props
     header, body = layout.panel("KHR_physics_rigid_bodies_importer", default_closed=False)
+    header.use_property_split = False
     header.prop(importProps, 'enabled')
     header.active = importProps.enabled
 
